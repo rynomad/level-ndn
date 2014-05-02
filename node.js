5bf156e44c74b9ca88325fb64447343d0fe7ccbf
Ryan Bennett <nomad.ry@gmail.com>
2014-05-01 18:15:21 -0600

    use sublevels, duh

--- node.js
+++ node.js
@@ -1,13 +1,17 @@
 var repo = {}
   , ndnr = require("./lib/ndn-repo.js")
   , down = false
-  , streamTransport = ("ndn-tcpServerTransport")
 
 repo.ndnr = ndnr
 repo.ndnr.levelDown(false)
 
 repo.tangle = function (stringprefix, a1, a2, ack) {
   repo.ndnr.initFace(stringprefix, null,null, ack)
+}
+
+if (process.argv.length > 2) {
+  console.log('argv[2] ', process.argv[2] )
+  repo.tangle(process.argv[2], null, null, function(){ console.log("repo tangled")})
 }
 
 module.exports = repo
